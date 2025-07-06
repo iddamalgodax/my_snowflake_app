d184bafd2c0a403344e2859b7424e21d52ecbb80
Sandun Iddamalgoda <136474612+iddamalgodax@users.noreply.github.com>
2025-07-06 18:51:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import pandas as pd
 import snowflake.snowpark as snowpark
 
 def main():
     session = snowpark.Session.builder.configs(st.secrets["snowflake"]).create()
-    df = session.sql("SELECT CURRENT_DATE as today").to_pandas()
-    
-    st.title("🚀 My Streamlit App in Snowflake")
-    st.write("Today's date from Snowflake:")
-    st.dataframe(df)
+    df = session.sql("SELECT * FROM MY_DB.MY_SCHEMA.MY_TABLE").to_pandas()
+
+    st.title("🚀 External Streamlit App")
+    st.write(df)
 
 if __name__ == "__main__":
     main()
